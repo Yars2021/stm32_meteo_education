3f1758556c114c915743e4998827357078c536a6
Ja_Vani <Sonic_Van@mail.ru>
2023-08-07 13:35:55 +0300

    New version

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,13 +2,15 @@
 
 #include "include.h"
 
+extern SPI_HandleTypeDef spi_2;
+
 int main (void)
 {
     initLowLevel();
 
     temputer_sensor sen;
 
-    init_temp(&sen, 0);
+    init_temp(&sen, GPIOB, GPIO_PIN_12, &spi_2);
     while (1)
     {
         get_temp(&sen);
--- Core/Src/spi_sense.c
+++ Core/Src/spi_sense.c
@@ -11,9 +11,9 @@ void init_temp(temputer_sensor *sen, GPIO_TypeDef *CS_GPIOx, uint16_t CS_GPIO_Pi
 
 void get_temp(temputer_sensor *sen){
     char out[2] = {0};
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
-    HAL_SPI_TransmitReceive(sen->spi, out, sen->raw_data, 2, 350);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(sen->Interface.CS_GPIOx, sen->Interface.CS_GPIO_Pin, GPIO_PIN_RESET);
+    HAL_SPI_TransmitReceive(sen->Interface.spi, out, sen->raw_data, 2, 350);
+    HAL_GPIO_WritePin(sen->Interface.CS_GPIOx, sen->Interface.CS_GPIO_Pin, GPIO_PIN_SET);
     uint16_t raw = sen->raw_data[1]>>5 | sen->raw_data[0]<<3;
     sen->temp = raw * 0.25;
     if(sen->temp > 128.0) sen->temp = 128.0 - sen->temp; 
